65d14f0111aa7703fae886f0cdfd393516c3cf1d
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-10-09 15:38:54 +0400

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="h-screen w-full bg-sky-100">
+      <div className="flex items-center ">
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
