e8eea755fe2be14a97ea42e061e51d0caa6b797c
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-10-09 18:38:04 +0400

    add: logo on Umbrella

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,39 @@ import pinkUmbrella from "./assets/Umbrella/Pink umbrella.png";
 import yellowUmbrella from "./assets/Umbrella/Yello umbrella.png";
 import uploadLogo from "../public/upload_icon.svg";
 import { useState } from "react";
+import axios from "axios";
 function App() {
   const [selectedColor, setSelectedColor] = useState("blue");
+  const [uploadedLogo, setUploadedLogo] = useState("");
+  const [logoTitle, setLogoTitle] = useState("");
 
-  console.log(selectedColor);
+  const getLogoUrl = async (logo) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", logo);
+      const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMGBB_API_KEY
+        }`,
+        formData
+      );
+      setLogoTitle(data.data.title);
+      return data.data.display_url;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const handleUploadLogo = async (e) => {
+    const logo = e.target.files[0];
+    // console.log(logo);
+    try {
+      const logoUrl = await getLogoUrl(logo);
+      setUploadedLogo(logoUrl);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  console.log(uploadedLogo);
   return (
     <div
       className={`h-screen w-full flex items-center justify-center ${
@@ -20,7 +49,7 @@ function App() {
       }`}
     >
       <div className="w-[80%] flex flex-col md:flex-row items-center justify-center gap-20">
-        <div>
+        <div className="relative">
           <img
             className="w-[350px]"
             src={
@@ -34,6 +63,14 @@ function App() {
             }
             alt=""
           />
+          <div className="absolute bottom-6 left-1/2 overflow-hidden w-20 h-5 -translate-x-[50%]">
+            {uploadedLogo && (
+              <img
+                className="object-cover h-full w-full"
+                src={uploadedLogo}
+              ></img>
+            )}
+          </div>
         </div>
         <div>
           <h2 className="text-3xl font-semibold mb-6">Custom Umbrella</h2>
@@ -66,13 +103,33 @@ function App() {
               .png and .jpg files only. Max file size is 5MB
             </span>
           </div>
-          <div className="mt-6 py-2 px-4 rounded-md text-white font-medium bg-sky-400">
-            <button className="uppercase flex items-center">
-              <span className="w-16">
-                <img className="w-5" src={uploadLogo} alt="" />
+          <div className="mt-6 py-2 px-4 rounded-md text-white font-medium">
+            <label
+              htmlFor="logo"
+              className={`flex items-center justify-center w-full gap-6 ${
+                selectedColor === "blue"
+                  ? "bg-blue-500"
+                  : selectedColor === "pink"
+                  ? "bg-pink-500"
+                  : selectedColor === "yellow"
+                  ? "bg-yellow-500"
+                  : ""
+              } text-white py-3 rounded cursor-pointer shadow-lg transition-colors duration-300`}
+            >
+              {/* Upload icon */}
+              <span>
+                <img src={uploadLogo} className="w-5" alt="Upload Logo" />
               </span>
-              Upload Logo
-            </button>
+              {logoTitle ? logoTitle.slice(0, 15) + "..." : "UPLOAD LOGO"}
+            </label>
+            <input
+              type="file"
+              id="logo"
+              accept="image/*"
+              name="logo"
+              className="hidden"
+              onChange={handleUploadLogo}
+            />
           </div>
         </div>
       </div>
